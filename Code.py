c938f603b38088f80fbe0112a9afb77096c5e62c
SkeyRahaman <sakibmondal7@gmail.com>
2019-10-24 20:04:05 +0530

    update

---
+++ Code.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from KNearestNeighbors import KNearestNeighbours
+from sklearn.metrics import accuracy_score
+
+
+data = sns.load_dataset("iris")
+x = data.iloc[:, 0:4].values
+y = data.iloc[:, -1].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.25)
+knn = KNearestNeighbours(n_neighbors=11)
+knn.fit(x_train=x_train,y_train=y_train)
+
+y_pred = knn.predict(np.array(x_test).reshape(len(x_test), len(x_test[0])))
+
+print(accuracy_score(y_test,y_pred))
+
+
+
---
+++ KNearestNeighbors.py
@@ -0,0 +1,36 @@
+import operator
+from collections import Counter
+
+
+class KNearestNeighbours:
+    def __init__(self, n_neighbors):
+        self.n_neighbors = n_neighbors  # n_neighbors is the number of n n_neighbors with which the comparison will take plae.
+        self.result = []                # result store the overall result result of this algorithm.
+        self.x_train = []               # x_trane and y_trane is the tranning data.
+        self.y_train = []
+
+    def fit(self, x_train, y_train):
+        self.x_train = x_train          # Stores the tranning data.
+        self.y_train = y_train          # Stores the tranning result.
+        print("Tranning done!")
+
+    def predict(self, x_test):      # x_test is the testing data.
+        for j in x_test:            # Loops around each value in x_test or number of values the algorithm need to predict.
+            distance = {}           # Distance stores all the distance between each point in x_traie and the test point
+            counter = 0             # keep a track of index number of x_trane to extract the respective result from y_trane
+            for i in self.x_train:  # Loops around each value in x_trane or number of diamention this algorithm will use.
+                sum_of_the_square = 0   # Stores the incomplete distance between two point.
+                for k in range(len(j)):
+                    sum_of_the_square += (j[k] - i[k]) ** 2
+                distance[counter] = sum_of_the_square ** 1 / 2
+                counter += 1
+            distance = sorted(distance.items(), key=operator.itemgetter(1))
+            self.result.append(self.classify(distance[:self.n_neighbors]))    # Classified result is stored in required variable which will be returned to the user.
+            del distance
+        return self.result
+
+    def classify(self, distance):   #Derive the result of this algorithm.
+        label = []
+        for i in distance:
+            label.append(self.y_train[i[0]])
+        return Counter(label).most_common()[0][0]
